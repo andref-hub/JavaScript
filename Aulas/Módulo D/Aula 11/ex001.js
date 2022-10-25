5fea5d604e119b7a1dc3a33a70c5ff41c6f0063c
andref-hub <114247626+andref-hub@users.noreply.github.com>
2022-10-25 12:53:27 -0300

    Módulo D - Aula 11

---
+++ Aulas/Módulo D/Aula 11/ex001.js
@@ -0,0 +1,8 @@
+console.log('O console está funcionando!')
+
+var vel = 100
+console.log(`A velocidade de seu carro é ${vel}km/h`)
+//Condição simples
+if (vel > 90) {
+    console.log("Você ultrapassou a velocidade permitida!")
+}
---
+++ Aulas/Módulo D/Aula 11/ex002.js
@@ -0,0 +1,10 @@
+console.log('O console está funcionando!')
+
+var origem = 'EUA'
+console.log(`Seu país de origem é: ${origem}`)
+//Condição composta
+if (origem == 'Brasil') {
+    console.log("Você é brasileiro (se fudeu)")
+} else {
+    console.log('Você é estrangeiro!')
+}
