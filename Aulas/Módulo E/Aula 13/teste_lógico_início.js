759755575f77ad1c747774627be40abe59f1ab8a
andref-hub <114247626+andref-hub@users.noreply.github.com>
2022-11-01 09:54:19 -0300

    Módulo E - Aula 13

---
+++ Aulas/Módulo E/Aula 13/teste_lógico_início.js
@@ -0,0 +1,24 @@
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++ // c <- c+1
+}
+
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++ // c <- c+1
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
