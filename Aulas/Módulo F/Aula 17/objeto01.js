fe5e5b8c0d1050381196e3419e961b485b025abb
andref-hub <114247626+andref-hub@users.noreply.github.com>
2022-11-10 17:32:10 -0300

    Módulo F - Objetos (demonstração)

---
+++ Aulas/Módulo F/Aula 17/objeto01.js
@@ -0,0 +1,11 @@
+let amigo = {
+    nome: 'José', 
+    peso: 85.4, 
+    engordar(p=0) {
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+console.log(typeof amigo)
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}`)
